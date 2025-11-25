48a917726ae564fe11735b7cdb9bc64e738ba339
granitraf <granitraf@gmail.com>
2025-11-24 20:03:47 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import routeTree from './routeTree.gen'
 import './styles/global.css'
 
 // Create the router instance
